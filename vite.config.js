a98a8b4ed182c023c61f6ddc88e57f3afda9e5c1
eugenesay0511 <eugene_say@apecia.com>
2026-01-29 15:11:19 +0800

    add github pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/ar-namecard/",
   server: {
     allowedHosts: ["jerlene-uncoaxed-trebly.ngrok-free.dev"],
   },
